86c12af7ff092eb287fc09835fe08f75d43ca9dc
Allan <sky.dev212@gmail.com>
2020-04-02 16:30:01 -0300

    make it better

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express')
+
+
+const app = express()
+
+
+const http = require('http').Server(app)
+
+const io = require('socket.io')(http)
+
+const port = process.env.Port || 3000
+
+app.use(express.static(__dirname + '/public'))
+
+let clients = 0
+
+
+io.on('connection' , function(socket){
+  socket.on("NewClient" , function(){
+    clients++
+    if(clients < 2){
+      if(clients == 1){
+        this.emit('CreatePeer')
+      }
+    }
+    else
+      this.emit('SessionActive')
+
+    console.log(clients)
+  })
+  socket.on('Offer', SendOffer)
+  socket.on('Answer', SendAnswer)
+  socket.on('disconnect', Disconnect)
+})
+
+
+function Disconnect(){
+  if(clients > 0){
+    if(clients <= 2)
+      this.broadcast.emit('Disconnect')
+    console.log()
+      --clients
+  }
+
+}
+
+function SendAnswer(data){
+  this.broadcast.emit('BackAnswer', data)
+}
+
+function SendOffer(offer){
+  this.broadcast.emit('BackOffer', offer)
+}
+
+http.listen(port, () => console.log(`Active on ${port}`))
+
+
+
+
+
